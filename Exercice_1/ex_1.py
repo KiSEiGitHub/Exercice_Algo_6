476f896127f4cb9e906f76363d707e06a28539f7
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-21 21:01:52 +0200

    Séquance petit 1 / petit 2

--- Exercice_1/ex_1.py
+++ Exercice_1/ex_1.py
@@ -30,11 +30,54 @@ class calcul:
         print(f's: {d}')
         print(f't: {e}')
 
+
+class calcul2:
+    def __init__(self):
+        self.x = 2
+        self.y = -2
+        self.z = -4
+        self.s = 1
+        self.t = 3
+
+    def calc_t(self):
+        return - self.y
+
+    def calc_z(self):
+        return self.t ** 2
+
+    def calc_x(self):
+        return self.z - self.x
+
+    def calc_t2(self):
+        return self.x
+
+    @staticmethod
+    def calc_y():
+        return 8
+
+    @staticmethod
+    def affich(a, b, c, e):
+        print(f'x: {a}')
+        print(f'y: {b}')
+        print(f'z: {c}')
+        print(f's: 0')
+        print(f't: {e}')
+
+
 def main():
+    print('Calcul 1')
     clc = calcul()
     clc.affich(
         clc.calc_x(), clc.calc_y(), clc.calc_z(),
         clc.calc_s(), clc.calc_t()
+    )
+    print('-' * 50)
+
+    print('Calcul 2')
+    clc2 = calcul2()
+    clc2.affich(
+        clc2.calc_x(), clc2.calc_y(), clc2.calc_z(),
+        clc2.calc_t()
     )
 
 
