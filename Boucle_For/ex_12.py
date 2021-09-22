95c542e79083020b89fe16188c7ad37a801d1b56
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-22 10:15:13 +0200

    capital, taux, année

--- Boucle_For/ex_12.py
+++ Boucle_For/ex_12.py
@@ -1,16 +1,34 @@
-def capital(argent, annee, taux):
-    for _ in range(1, annee):
-        argent += 80 * taux / 100
-    return argent
+class capital:
+    def __init__(self, argent, taux, annee):
+        self.argent = argent
+        self.taux = taux
+        self.annee = annee
+
+    def capital1(self):
+        for _ in range(1, self.annee):
+            self.argent += 80 * self.taux / 100
+        return self.argent
+
+    def capital2(self):
+        a = 0
+        somme = 0
+        while somme <= self.argent * 2:
+            somme += self.argent + 80
+            a += 1
+        return a
 
 
 def main():
     while True:
         argent = float(input('argent >> '))
         annee = int(input('années >> '))
-        taux = float(input('taux >> '))
-        somme = capital(argent, annee, taux)
-        print(f'somme total au bout de {annee} ans: {round(somme, 2)} €')
+        taux = float(input('Taux >> '))
+
+        resultat = capital(argent, taux, annee)
+
+        print(f'Somme total au bout de {annee} ans: {round(resultat.capital1(), 2)} €')
+        print(f'La somme aura doubler en {resultat.capital2()} ans')
+
         print('-' * 50)
 
 
